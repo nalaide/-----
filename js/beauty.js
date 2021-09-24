8535c5b72febdcd9e48b8916832be9a530d811bb
jesus <2540016657@qq.com>
2021-09-24 09:39:05 +0800

    第一次提交的大商创页面

---
+++ js/beauty.js
@@ -0,0 +1,82 @@
+// 个人化妆、清洁用品
+$('.clean-drop-z').mouseover(function() {
+    $(this).addClass('clean-drop-block-z');
+});
+$('.clean-drop-none-z').mouseout(function() {
+    $('.clean-drop-z').removeClass('clean-drop-block-z');
+});
+// 主体部分---品牌
+$('.selector-pic-main-z li').hover(function() {
+    $(this).find('img').css('display', 'none');
+    $(this).find('span').css('border-color', '#f42424');
+}, function() {
+    $(this).find('img').css('display', 'block');
+    $(this).find('span').css('border-color', 'white');
+});
+//主体部分---多选单选
+$('.icon-down').click(function() {
+    $(this).toggleClass('icon-up');
+});
+$('.selector-checkbox-r-z').click(function() {
+    $(this).find('.icon-plus').toggleClass('icon-reduce');
+    $(this).find('span').toggleClass('mul-z').text('收起');
+});
+//主体部分---所有品牌切换
+$('.all-a-z li').mouseover(function() {
+    $(this).addClass('curr-z').siblings().removeClass('curr-z');
+});
+$('.selector-zimu-z li span').click(function() {
+    $(this).toggleClass('span-icon-z');
+});
+$('.selector-close-btn-z span').click(function() {
+    $(this).parents('.selector-letter-z').css('display', 'none');
+    $('.selector-pic-main-z').css('display', 'block');
+});
+$('.selector-checkbox-r-z').click(function() {
+    $('.selector-pic-main-z').css('display', 'none');
+    $('.selector-letter-z').css('display', 'block');
+});
+// 输入框的清空确定的出现消失
+$('.filter-warp-sub-z input').focusin(function() {
+    $('.filter-warp-sure-z').css('display', 'block');
+});
+$('.filter-warp-sub-z input').focusout(function() {
+    $('.filter-warp-sure-z').css('display', 'none');
+});
+$('.filter-warp-fcheckbox-z label').click(function() {
+    console.log($(this));
+    $(this).toggleClass('label-z');
+});
+//图片切换
+$('.goods-list-sider-z li').mouseenter(function() {
+    var imgArr = $(this).find('img');
+    var repImg = $(this).parents('.goods-list-pic-z').find('.goods-list-img-z').find('img');
+    repImg.attr('src', imgArr.attr('src'));
+});
+//收藏的红心切换
+$('.icon-zan-alt').parent('a').click(function() {
+    $(this).children().toggleClass('icon-zan-alts');
+    $(this).css('color', '#f42424');
+    if (!$(this).children().hasClass('icon-zan-alts')) {
+        $(this).css('color', '#555');
+    };
+});
+// 对比
+$('.goods-list-ope-z input').click(function() {
+    if ($(this).is(':checked')) {
+        $(this).next().addClass('hook-z');
+        $('.duibi-block-z').fadeIn();
+    } else {
+        $(this).next().removeClass('hook-z');
+        $('.duibi-block-z').fadeOut();
+    }
+});
+//清空对比栏
+$('.second-z span').click(function() {
+    console.log($(this));
+});
+//隐藏对比栏
+$('.third-z').click(function() {
+    $(this).parents('.duibi-block-z').fadeOut();
+});
+// console.log(page1);
---
+++ js/dataf.js
@@ -0,0 +1,265 @@
+var dataf1 = [{
+    src: '../images/001-f.jpg',
+    title: '摩飞（Morphyrichards）榨汁机 便携式充电迷你无线果汁机料理机随行杯MR9800 蓝色',
+    pricelow: '¥219.00',
+    price: '¥399.00'
+}, {
+    src: '../images/002-f.jpg',
+    title: '合生元Razer雷蛇 雨林狼蛛幻彩版 Ornata Chroma 机械式薄膜游戏键盘',
+    pricelow: '¥500.00',
+    price: '¥600.00'
+}, {
+    src: '../images/003-f.jpg',
+    title: '喜瑞新款学院风韩版时尚太空棉宽松长袖印花圆领卫衣女',
+    pricelow: '¥233.00',
+    price: '¥279.59'
+}, {
+    src: '../images/004-f.jpg',
+    title: '华帝新款韩版chic学生宽松短款外套上衣字母长袖连帽套头卫衣女潮',
+    pricelow: '¥300.00',
+    price: '¥360.00'
+}, {
+    src: '../images/005-f.jpg',
+    title: '鸿星尔克韩都衣舍2017韩版女装新款黑白拼接插肩棒球服春季短外套HH5597妠 朴信惠同款 黑白拼接 插肩袖 棒球服',
+    pricelow: '¥450.00',
+    price: '¥540.00'
+}, {
+    src: '../images/006-f.jpg',
+    title: '工银瑞信Samsung/三星 Galaxy C9 Pro SM-C9000 6+64G全金属超薄手机 12期免息 送蓝牙音箱等多种套餐好礼',
+    pricelow: '¥3500.00',
+    price: '¥4200.00'
+}, {
+    src: '../images/007-f.jpg',
+    title: '迪士尼伊米妮2017春新款简约时尚自然摔牛皮单肩手提大小版波士顿包女包 简约时尚 自然摔牛皮 单肩手提 波士顿包',
+    pricelow: '¥888.00',
+    price: '¥1065.60'
+}, {
+    src: '../images/008-f.jpg',
+    title: '汤臣倍健春季马丁靴男真皮男靴黄靴工装军靴韩版短靴沙漠靴高帮男鞋大黄靴 头层牛皮',
+    pricelow: '¥1000.00',
+    price: '¥1200.00'
+}, {
+    src: '../images/009-f.jpg',
+    title: '匡威Apple/苹果 Apple Watch Series 2 智能手表42mm',
+    pricelow: '¥3188.00',
+    price: '¥3825.60'
+}, {
+    src: '../images/010-f.jpg',
+    title: '磨铁图书DIY台式电脑整机I7四核六核独显固态组装游戏电脑主机24寸显示器 送百元礼品 内存免费升级 送显示器',
+    pricelow: '¥1388.00',
+    price: '¥1665.60'
+}, {
+    src: '../images/011-f.jpg',
+    title: '姬芮NewEra纽亦华 新款MLB棒球帽男女字母NY洋基LA道奇调节平沿嘻哈帽 官方正品',
+    pricelow: '¥219.00',
+    price: '¥262.80'
+}, {
+    src: '../images/012-f.jpg',
+    title: 'YOHO有货潮牌LAL/数字贴布连帽套头卫衣男女通用 吴亦凡亲着同款 春夏焕新季，3.21日00:00开始抢购',
+    pricelow: '¥159.00',
+    price: '¥190.79'
+}, {
+    src: '../images/013-f.jpg',
+    title: '康比特森马针织衫 冬季男圆领套头青年毛衣小清新线衫毛衫纯色韩版学生',
+    pricelow: '¥79.00',
+    price: '¥94.80'
+}, {
+    src: '../images/014-f.jpg',
+    title: '飞利浦TOSOT/大松 GDF-2001C格力电饭煲迷你智能全自动家用 电饭锅1-2人 格力出品 数码显示 智能预约 全国包邮',
+    pricelow: '¥388.00',
+    price: '¥465.59'
+}, {
+    src: '../images/015-f.jpg',
+    title: '乔山洽福 转椅轮子万向轮 老板椅办公椅子滑轮电脑椅配件滚轮脚轮轱辘 静音灵活不伤地板质保三年下单备注安装方式',
+    pricelow: '¥23.80',
+    price: '¥28.56'
+}, {
+    src: '../images/016-f.jpg',
+    title: '猫人Xiaomi/小米 小米电视4A 55英寸 4k超高清智能网络电视机 50 60',
+    pricelow: '¥2399.00',
+    price: '¥2878.79'
+}, {
+    src: '../images/017-f.jpg',
+    title: '欧亚马山水 F7 5.1家庭影院音响套装电视客厅家用3d环绕组合音箱 电视k歌音响套装 家用 组合 重低音蓝牙',
+    pricelow: '¥1999.00',
+    price: '¥2398.79'
+}, {
+    src: '../images/018-f.jpg',
+    title: '小米空气净化器2S家用办公智能氧吧除甲醛雾霾粉尘 新品',
+    pricelow: '¥800.00',
+    price: '¥960.00'
+}, {
+    src: '../images/019-f.jpg',
+    title: '小米8SE全面屏拍照游戏智能手机学生机商务机8周年 官方旗舰店正品现货速发',
+    pricelow: '¥1888.00',
+    price: '¥2265.60'
+}, {
+    src: '../images/020-f.jpg',
+    title: '小熊（Bear）咖啡机 美式家用 0.7L全自动滴漏式小型泡茶煮咖啡壶 KFJ-A07V1',
+    pricelow: '¥129.00',
+    price: '¥299.00'
+}, {
+    src: '../images/021-f.jpg',
+    title: '东菱 Donlim 多功能早餐机 三明治机 华夫饼机 多士炉面包机 电火锅轻食机 可做甜甜圈蛋挞 D',
+    pricelow: '¥399.00',
+    price: '¥599.00'
+}, {
+    src: '../images/022-f.jpg',
+    title: '华为 HUAWEI Mate 30 麒麟990旗舰芯片4000万超感光徕卡影像屏内指纹8G+128G',
+    pricelow: '¥4299.00',
+    price: '¥10000.00'
+}, {
+    src: '../images/023-f.jpg',
+    title: '匡威马克华菲短袖T恤 夏装新款舒适圆领条纹拼接男简约修身短袖上衣',
+    pricelow: '¥128.00',
+    price: '¥153.60'
+}, {
+    src: '../images/024-f.jpg',
+    title: '金史密斯HLA/海澜之家撞色长袖T恤春季热卖圆领修身拼接T恤男 简约圆领 微弹修身 撞色拼接 触感柔软',
+    pricelow: '¥98.00',
+    price: '¥117.60'
+}, {
+    src: '../images/025-f.jpg',
+    title: '杰克琼斯马克华菲长袖T恤男 冬季新品纯棉圆领黑白潮款印花休闲t恤   98',
+    pricelow: '¥80.00',
+    price: '¥96.00'
+}]
+var dataf2 = [{
+    src: '../images/01-f.jpg',
+    title: '麦斯威尔杰威尔发胶定型喷雾男士干胶头发持久定型造型啫喱水发蜡蓬松清香 快速定型，蓬松清香，不起白屑，',
+    price: '¥0.01',
+    count: '5'
+}, {
+    src: '../images/02-f.jpg',
+    title: '测试',
+    price: '¥50.00',
+    count: '11'
+}, {
+    src: '../images/03-f.jpg',
+    title: 'Maybelline/美宝莲好气色水润唇部套装 滋润保湿持久显色唇露唇膏',
+    price: '¥100.00',
+    count: '1'
+}, {
+    src: '../images/04-f.jpg',
+    title: '小米宝莲 透净瞬洁净妆乳 120ml 卸妆乳 卸妆液卸妆无残留专柜正品 安全温和 彻底清洁 水润清爽',
+    price: '¥100.00',
+    count: '3'
+}, {
+    src: '../images/05-f.jpg',
+    title: '宝姿美宝莲 好气色潮色双搭轻垫霜提亮肌肤白皙气垫CC霜凸显好气色 提亮肌肤',
+    price: '¥100.00',
+    count: '0'
+}, {
+    src: '../images/06-f.jpg',
+    title: '宝姿美宝莲 奇妙净颜盈润洁面膏100ml 保湿 控油 滋润 祛除多余油脂 不干燥 不紧绷',
+    price: '¥100.00',
+    count: '0'
+}, {
+    src: '../images/07-f.jpg',
+    title: '宝姿美宝莲 色秀指甲油 糖果色 专业彩妆美甲 专柜正品 绚丽色彩 多色可选',
+    price: '¥100.00',
+    count: '0'
+}, {
+    src: '../images/08-f.jpg',
+    title: '宝姿Maybelline/美宝莲净澈多效卸妆水清爽型 舒缓 深层清洁 舒缓 补水 深层清洁 卸妆',
+    price: '¥100.00',
+    count: '0'
+}, {
+    src: '../images/09-f.jpg',
+    title: '宝姿美宝莲宝蓓护唇膏 宝贝护唇膏 润唇膏 唇彩口红保湿 滋润',
+    price: '¥100.00',
+    count: '0'
+}, {
+    src: '../images/10-f.jpg',
+    title: '宝姿美宝莲 宝蓓爱炫彩护唇膏 润唇膏 波普风 保湿滋润 显色丰润 一扫唇色暗沉 卖萌必备',
+    price: '¥100.00',
+    count: '0'
+}, {
+    src: '../images/11-f.jpg',
+    title: '佳沪数码-华为欧莱雅男士水能保湿化妆护肤品套装深层补水滋润洗面奶爽肤水乳液',
+    price: '¥60.00',
+    count: '1'
+}, {
+    src: '../images/12-f.jpg',
+    title: '皮克朋一叶子去角质去死皮膏啫喱面部温和祛黑头全身温和补水凝胶磨砂膏 新鲜护肤 植物护肤 去除角质 光滑柔嫩',
+    price: '¥108.00',
+    count: '1'
+}, {
+    src: '../images/13-f.jpg',
+    title: 'NINE WEST花美时三合一自动旋转双头眉笔眉粉染眉膏画眉持久防水防汗不脱色 防水防汗 持久不晕染 正品包邮',
+    price: '¥200.00',
+    count: '0'
+}, {
+    src: '../images/14-f.jpg',
+    title: '飞利浦美宝莲绝色持久唇膏 粉红警报 魅惑炫亮润泽 唇彩口红',
+    price: '¥300.00',
+    count: '17'
+}, {
+    src: '../images/15-f.jpg',
+    title: 'justyle美宝莲绝色持久唇膏 粉红警报 魅惑炫亮润泽 唇彩口红',
+    price: '¥520.00',
+    count: '0'
+}, {
+    src: '../images/16-f.jpg',
+    title: '宝姿美宝莲 顺滑恒久眉笔流畅线条 利落型眉 可长久持妆 新品上市 流畅线条 利落型眉 长效持妆',
+    price: '¥100.00',
+    count: '0'
+}];
+
+function laoddataf1(fatherbox) {
+    $(fatherbox).each(function(index, value) {
+        $('<a href="loupe.html" target="_black">').css({
+            'display': 'block',
+            'width': '100 %',
+            'height': '100 %',
+            'text-decoration': 'none',
+            'color': 'black'
+        }).addClass('a-f').appendTo($(value));
+        $('<div>').css({
+            'width': '210px',
+            'height': '210px'
+        }).addClass('img-f').appendTo($(value).find('.a-f'));
+        $('<img>').css({
+            'width': '100%',
+            'height': '100%',
+        }).attr('src', dataf1[index].src).appendTo($(value).find('.img-f'));
+        $('<p>' + dataf1[index].title + '</p>').css({
+            'overflow': 'hidden',
+            'white-space': 'nowrap',
+            'text-overflow': 'ellipsis',
+            'font-size': '14px',
+            'color': '#555',
+            'margin': '10px 0'
+        }).attr('title', dataf1[index].title).appendTo($(value).find('.a-f'));
+        $('<sapn>').css({
+            'color': '#f42424',
+            'margin-right': '5px'
+        }).text(dataf1[index].pricelow).appendTo($(value).find('.a-f'));
+        $('<sapn>').css({
+            'font-size': '12px',
+            'text-decoration': 'line-through',
+            'color': '#555'
+        }).text(dataf1[index].pricelow).appendTo($(value).find('.a-f'));
+        $(value).hover(function() {
+            $(this).css('border-color', '#f42424')
+            $(this).find('p').css('color', '#f42424')
+        }, function() {
+            $(this).css('border-color', '#D2D2D2')
+            $(this).find('p').css('color', '#555')
+        })
+        $(value).children('.a-f')[0].href += '?' + $(value).find('img')[0].src.split('/')[$(value).find('img')[0].src.split('/').length - 1];
+    });
+};
+
+function laoddataf2(fatherbox) {
+    $(fatherbox).each(function(index, value) {
+        console.log($(this));
+        $(value).find('img').each(function(inde, valu) {
+            $(valu).attr('src', dataf2[index].src)
+        })
+        $(value).children('div').children('div').eq(0).html(dataf2[index].price)
+        $(value).find('b').text(dataf2[index].count)
+        $(value).find('p').attr('title', dataf2[index].title)
+        $(value).find('p').children('a').text(dataf2[index].title)
+    });
+};
